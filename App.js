630d0c4bf378e30e359f4ce7e66d34bf3d6d09ff
Mynul Islam <mynul.sakil@gmail.com>
2025-03-05 12:23:29 +0600

    video 20

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ export default function App() {
   return (
     <View style={styles.appContainer}>
      <View style={styles.inputContainer}> 
-      <TextInput placeholder="Your course goal" style={styles.textStyle}/>
+      <TextInput placeholder="Your course goal" style={styles.textInput}/>
       <Button title="ADD Goal"/>
-     </View>
+     </View> 
      <View>
       <Text>List Of Goal ...</Text>
      </View>
@@ -19,7 +19,18 @@ const styles = StyleSheet.create({
     padding: 50
   },
   inputContainer:{
-    
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor: '#cccccc',
+    width:'80%',
+    padding: 8,
+    borderRadius: 5 ,
+    marginLeft: 10
   }
 
 

e8e1282a09127779bb53791267bdd4febd57280a
Mynul Islam <mynul.sakil@gmail.com>
2025-03-05 12:15:29 +0600

    video 18

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, Text, View,Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.textStyle}>Another Piece of Text</Text>
-      </View>
-      <Text style={{margin:16,borderWidth:2,borderColor:'#22ff22',padding:16}}>Hello World</Text>   
-      <Button title="Click Me" onPress={()=>alert('Button Clicked')} />
+    <View style={styles.appContainer}>
+     <View style={styles.inputContainer}> 
+      <TextInput placeholder="Your course goal" style={styles.textStyle}/>
+      <Button title="ADD Goal"/>
+     </View>
+     <View>
+      <Text>List Of Goal ...</Text>
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50
   },
-  textStyle:{
-    fontSize:20,
-    color:'red'
+  inputContainer:{
+    
   }
+
 
 });
